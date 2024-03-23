8ff519c3f9f16b36a5645789ed7824470ae21101
Pau Carrera <poksy2002@gmail.com>
2024-03-23 17:57:05 +0100

    First introduction of the website
    First test of the folder organization and the drop-down menu with its html and css files.
    
    I hope to improve as I learn more knowledge both in class and self-taught.

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    const menuBtn = document.querySelector('.menu-btn');
+    
+    menu.classList.toggle('active');
+    menuBtn.classList.toggle('close');
+  }
+  
