a96dbd0e43dcbaa5017b957deba97097a3289c98
Pau Carrera <poksy2002@gmail.com>
2024-03-24 18:26:51 +0100

    logo idea + navigation menu
    My idea is to express urban creativity with programming.
    
    In the menu I think that in full window it should be a normal menu on the top right and in responsive a drop down menu.

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector('.menu');
-    const menuBtn = document.querySelector('.menu-btn');
-    
-    menu.classList.toggle('active');
-    menuBtn.classList.toggle('close');
-  }
-  
