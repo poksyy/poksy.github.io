8c5b9b062cb20dde1ade093f57467a9e627cee93
Pau Carrera <poksy2002@gmail.com>
2024-11-04 18:20:36 +0100

    STYLE(titles+nav): visual titles added without funcionality

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function changeImage(image, newSrc) {
     image.src = newSrc;
   }
+
+document.getElementById('language-toggle').onclick = function() {
+  const dropdownContent = document.querySelector('.dropdown-content');
+  dropdownContent.style.display = dropdownContent.style.display === 'flex' ? 'none' : 'flex';
+}
+
+function setLanguage(language){
+  alert('Idioma cambiado a: ${language}');
+}
